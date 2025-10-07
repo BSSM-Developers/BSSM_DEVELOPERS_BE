6c97fb0733699127c118ccccf64e2733b09963d2
pdh0128 <pdh0128a@gmail.com>
2025-10-07 20:50:28 +0900

    feat :: io exception handler

--- src/main/java/com/example/bssm_dev/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/bssm_dev/global/error/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ package com.example.bssm_dev.global.error;
 import com.example.bssm_dev.global.error.exception.ErrorCode;
 import com.example.bssm_dev.global.error.exception.GlobalException;
 import com.example.bssm_dev.common.util.HttpUtil;
+import io.jsonwebtoken.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +25,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(statusCode)
                 .body(errorResponse);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorResponse> ioExceptionHanlder(IOException e) {
+        log.error("IOException occurred: {}", e.getMessage(), e);
+        ErrorResponse errorResponse = HttpUtil.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "입출력 오류가 발생했습니다.");
+        return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(errorResponse);
     }
 
 }
