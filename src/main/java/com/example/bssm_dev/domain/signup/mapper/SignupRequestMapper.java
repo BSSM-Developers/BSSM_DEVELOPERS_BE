c032ded1d13b0c19a64241d5c6c84f9634f5b8f5
pdh0128 <pdh0128a@gmail.com>
2025-10-07 20:04:36 +0900

    fix :: 회원가입 신청 state

--- src/main/java/com/example/bssm_dev/domain/signup/mapper/SignupRequestMapper.java
+++ src/main/java/com/example/bssm_dev/domain/signup/mapper/SignupRequestMapper.java
@@ -1,12 +1,13 @@
 package com.example.bssm_dev.domain.signup.mapper;
 
+import com.example.bssm_dev.domain.signup.dto.request.SignupRequest;
 import com.example.bssm_dev.domain.signup.model.SignupForm;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SignupRequestMapper {
 
-    public SignupForm toSignupRequest(com.example.bssm_dev.domain.signup.dto.request.SignupRequest signupRequestDto) {
+    public SignupForm toSignupRequest(SignupRequest signupRequestDto) {
         return SignupForm.of(
                 signupRequestDto.name(),
                 signupRequestDto.email(),
--- src/main/java/com/example/bssm_dev/domain/signup/model/SignupForm.java
+++ src/main/java/com/example/bssm_dev/domain/signup/model/SignupForm.java
@@ -2,12 +2,16 @@ package com.example.bssm_dev.domain.signup.model;
 
 import com.example.bssm_dev.domain.signup.model.type.SignUpFormState;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Builder
+@AllArgsConstructor
 public class SignupForm {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,13 +24,12 @@ public class SignupForm {
     @Enumerated(EnumType.STRING)
     private SignUpFormState state;
 
-    public SignupForm(String name, String email, String profile) {
-        this.name = name;
-        this.email = email;
-        this.profile = profile;
-    }
-
     public static SignupForm of(String name, String email, String profile) {
-        return new SignupForm(name, email, profile);
+        return SignupForm.builder()
+                .name(name)
+                .email(email)
+                .profile(profile)
+                .state(SignUpFormState.PENDING)
+                .build();
     }
 }
