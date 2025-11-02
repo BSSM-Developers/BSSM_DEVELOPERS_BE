3ef989f28cac525d3fcf41abb9c205373137fd55
pdh0128 <pdh0128a@gmail.com>
2025-11-03 08:17:25 +0900

    fix :: jpa용 매핑 어노테이션 제거

--- src/main/java/com/example/bssm_dev/domain/docs/model/Docs.java
+++ src/main/java/com/example/bssm_dev/domain/docs/model/Docs.java
@@ -1,13 +1,11 @@
 package com.example.bssm_dev.domain.docs.model;
 
-import jakarta.persistence.Embedded;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Document(collection = "docs")
 public class Docs {
     private String id;
-    @Embedded
     private List<DocsBlock> docsBlocks;
 }
 
--- src/main/java/com/example/bssm_dev/domain/docs/model/DocsBlock.java
+++ src/main/java/com/example/bssm_dev/domain/docs/model/DocsBlock.java
@@ -1,8 +1,5 @@
 package com.example.bssm_dev.domain.docs.model;
 
-import jakarta.persistence.Embeddable;
-
-@Embeddable
 public class DocsBlock {
     private String id;
     private String module;
