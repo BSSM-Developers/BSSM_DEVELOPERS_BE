5165c3956a5cc3e6c48e2e560c371f1d8c97f344
pdh0128 <pdh0128a@gmail.com>
2025-11-03 22:29:45 +0900

    feat :: CustomDocsInitializer 불필요한 상수 제거

--- src/main/java/com/example/bssm_dev/domain/docs/init/CustomDocsInitializer.java
+++ src/main/java/com/example/bssm_dev/domain/docs/init/CustomDocsInitializer.java
@@ -1,26 +1,18 @@
 package com.example.bssm_dev.domain.docs.init;
 
 import com.example.bssm_dev.common.util.RandomNumberGenerateUtil;
-import com.example.bssm_dev.domain.docs.dto.request.CreateDocsSideBarRequest;
 import com.example.bssm_dev.domain.docs.model.DocsPage;
 import com.example.bssm_dev.domain.docs.model.DocsPageBlock;
 import com.example.bssm_dev.domain.docs.model.SideBar;
 import com.example.bssm_dev.domain.docs.model.SideBarBlock;
 import com.example.bssm_dev.domain.docs.model.type.DocsModule;
 import com.example.bssm_dev.domain.docs.model.type.SideBarModule;
-import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
 public class CustomDocsInitializer {
     private static final String DOCS_MAPPED_ID = "doc-1";
-    private static final String LABEL = "시작하기";
-    private static final SideBarModule SIDE_BAR_MODULE = SideBarModule.DEFAULT;
-
-    private static final DocsModule DOCS_MODULE = DocsModule.DOCS_1;
-    private static final String DOCS_PAGE_BLOCK_MAPPED_ID = "init-1";
-    private static final String DOCS_PAGE_BLOCK_CONTENT = "빈 문서입니다.";
 
     private static final List<SideBarBlock> SIDE_BAR_BLOCKS = Collections.singletonList(
             initSideBarBlock()
@@ -48,9 +40,9 @@ public class CustomDocsInitializer {
     private static DocsPageBlock initDocsPageBlock() {
         return DocsPageBlock.builder()
                 .id(RandomNumberGenerateUtil.generate(5))
-                .mappedId(DOCS_PAGE_BLOCK_MAPPED_ID)
-                .content(DOCS_PAGE_BLOCK_CONTENT)
-                .module(DOCS_MODULE)
+                .mappedId("init-1")
+                .content("빈 문서입니다.")
+                .module(DocsModule.DOCS_1)
                 .build();
     }
 
@@ -58,8 +50,8 @@ public class CustomDocsInitializer {
         return SideBarBlock.builder()
                 .id(RandomNumberGenerateUtil.generate(5))
                 .mappedId(DOCS_MAPPED_ID)
-                .label(LABEL)
-                .module(SIDE_BAR_MODULE)
+                .label("시작하기")
+                .module(SideBarModule.DEFAULT)
                 .build();
     }
 }
