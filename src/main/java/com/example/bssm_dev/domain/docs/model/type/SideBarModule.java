ead0d6dae64981dda2c2983c272485c95a87c25b
pdh0128 <pdh0128a@gmail.com>
2025-11-03 18:59:42 +0900

    refactor :: MAINTITLE -> MAIN_TITLE

--- src/main/java/com/example/bssm_dev/domain/docs/model/type/SideBarModule.java
+++ src/main/java/com/example/bssm_dev/domain/docs/model/type/SideBarModule.java
@@ -1,7 +1,7 @@
 package com.example.bssm_dev.domain.docs.model.type;
 
 public enum SideBarModule {
-    DEFAULT, SMALL, COLLAPSE, MAIN, MAINTITLE, API;
+    DEFAULT, SMALL, COLLAPSE, MAIN, MAIN_TITLE, API;
 
     public static SideBarModule fromString(String module) {
         return SideBarModule.valueOf(module.toUpperCase());
