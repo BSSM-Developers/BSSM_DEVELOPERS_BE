5e1eb33cdb4f372cfc742e37f96a1dec67064924
pdh0128 <pdh0128a@gmail.com>
2026-01-01 19:33:21 +0900

    feat :: Docs token count 필드 추가

--- app/src/main/java/com/example/bssm_dev/domain/docs/model/Docs.java
+++ app/src/main/java/com/example/bssm_dev/domain/docs/model/Docs.java
@@ -19,6 +19,8 @@ public class Docs {
     private DocumentType type;
     private String domain;
     private Long writerId;
+    @Builder.Default
+    private Long tokenCount = 0L;
 
     public boolean isMyDocs(User user) {
         return this.writerId.equals(user.getUserId());
