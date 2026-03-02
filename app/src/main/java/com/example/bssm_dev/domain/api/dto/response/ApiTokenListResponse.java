0784635c586b18d1972e7c897846998823048001
pdh0128 <pdh0128a@gmail.com>
2026-03-02 13:55:18 +0900

    fix :: api token 조회 시에, state도 반환

--- app/src/main/java/com/example/bssm_dev/domain/api/dto/response/ApiTokenListResponse.java
+++ app/src/main/java/com/example/bssm_dev/domain/api/dto/response/ApiTokenListResponse.java
@@ -1,8 +1,11 @@
 package com.example.bssm_dev.domain.api.dto.response;
 
+import com.example.bssm_dev.domain.api.model.type.ApiTokenState;
+
 public record ApiTokenListResponse(
         Long apiTokenId,
         String apiTokenName,
-        String apiTokenClientId
+        String apiTokenClientId,
+        ApiTokenState state
 ) {
 }
--- app/src/main/java/com/example/bssm_dev/domain/api/dto/response/ApiTokenResponse.java
+++ app/src/main/java/com/example/bssm_dev/domain/api/dto/response/ApiTokenResponse.java
@@ -1,11 +1,14 @@
 package com.example.bssm_dev.domain.api.dto.response;
 
+import com.example.bssm_dev.domain.api.model.type.ApiTokenState;
+
 import java.util.List;
 
 public record ApiTokenResponse(
         Long apiTokenId,
         String apiTokenName,
         String apiTokenClientId,
+        ApiTokenState state,
         List<String> domains,
         List<ApiUsageSummaryResponse> registeredApis
 ) {
