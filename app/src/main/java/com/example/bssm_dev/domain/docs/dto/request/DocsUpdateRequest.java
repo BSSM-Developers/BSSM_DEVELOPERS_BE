b31a54987c78690c4d15c6406bbdc988497b3ca0
pdh0128 <pdh0128a@gmail.com>
2026-03-04 09:47:48 +0900

    refactor :: docs path 시, domain과 repository url optional

--- app/src/main/java/com/example/bssm_dev/domain/docs/dto/request/DocsUpdateRequest.java
+++ app/src/main/java/com/example/bssm_dev/domain/docs/dto/request/DocsUpdateRequest.java
@@ -14,9 +14,7 @@ public record DocsUpdateRequest(
         String title,
         @NotBlank(message = "문서 설명은 필수입니다.")
         String description,
-        @NotBlank(message = "도메인은 필수입니다.")
         String domain,
-        @NotBlank(message = "깃허브 레포지토리 URL은 필수입니다.")
         String repositoryUrl,
 
         @NotNull(message = "sidebar는 null일 수 없습니다")
