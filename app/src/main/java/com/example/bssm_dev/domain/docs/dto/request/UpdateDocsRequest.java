e1ef5bf06851f80040e98e994b6beae2c3efe624
pdh0128 <pdh0128a@gmail.com>
2026-03-29 02:29:18 +0900

    refacotr :: domain, repo url 필수 제거

--- app/src/main/java/com/example/bssm_dev/domain/docs/dto/request/UpdateDocsRequest.java
+++ app/src/main/java/com/example/bssm_dev/domain/docs/dto/request/UpdateDocsRequest.java
@@ -7,9 +7,7 @@ public record UpdateDocsRequest(
         String title,
         @NotBlank(message = "문서 설명은 필수입니다.")
         String description,
-        @NotBlank(message = "도메인은 필수입니다.")
         String domain,
-        @NotBlank(message = "깃허브 레포지토리 URL은 필수입니다.")
         String repositoryUrl
 ) {
 }
